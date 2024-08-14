c8829144111f87599511b055da09bac871b7791b
Gwers1 <lnathanu@gmail.com>
2024-08-15 01:03:49 +1000

    Finished testing all data is avaliable

--- test.py
+++ test.py
@@ -2,14 +2,21 @@ import torch
 import torchvision
 import glob
 import cv2
-import matplotlib.pyploy as plt
+import matplotlib.pyplot as plt
 
 def show_images(dataset, num_samples=20, cols=4):
     """ Plots somes samples from dataset """
     plt.figure(figsize=(15,15))
-    for i, img in enumerate(data):
+    for i in range(0, num_samples):
         if i == num_samples:
             break
-        plt.subplt(num_samples/cols + 1, cols, i + 1)
-        plt.imshow(img[0])
+        plt.subplot(int(num_samples/cols + 1), cols, i + 1)
+        plt.imshow(dataset[i])
+    plt.show()
+        
 
+file = 'resources/heightmaps/*.png'
+images = [cv2.imread(image) for image in glob.glob(file)]
+print(type(images[0]))
+plt.imshow(images[0])
+show_images(images)
